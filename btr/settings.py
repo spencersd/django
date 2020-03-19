afe271167129986a63a339cc594ea67ccc9f6369
Spencer Barton-fisher <spencerbarton-fisher@MacBook-Pro-2.modem>
2020-03-20 06:10:30 +1100

    database setup and remove print statements

--- btr/settings.py
+++ btr/settings.py
@@ -69,7 +69,6 @@ TEMPLATES = [
         },
     },
 ]
-print(TEMPLATES)
 
 WSGI_APPLICATION = 'btr.wsgi.application'
 
@@ -79,8 +78,11 @@ WSGI_APPLICATION = 'btr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btrdb',
+        'USER': 'postgres',
+        'PASSWORD': 'Spencbf1',
+        'HOST': 'localhost'
     }
 }
 
